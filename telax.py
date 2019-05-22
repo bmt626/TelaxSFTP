0b87c5b1e674f80890f2edd2c0882eec7911caf8
Brandon Taylor <KDBTA@kumon.com>
2019-05-22 09:48:07 -0400

    moved to while loop instead of qutting after three errors

--- telax.py
+++ telax.py
@@ -4,21 +4,12 @@ import time
 
 import pysftp
 
+from connectinfo import TELAXSERVER, TELAXPWD, TELAXUSER, LOCALDIR, KH, LOGDIR
 from pyemail import sendemail
 
-TELAXSERVER = "FTPSERVER"
-TELAXUSER = "USERNAME"
-TELAXPWD = "PASSWORD"
-LOCALDIR = "LOCALHOST_DIR"
-# pysftp does not auto add known hosts I to sftp in from a server (linux)
-# then copy my known host to a new file specified here
-KH = "PATHTOKNOWNHOSTSFILE"
-LOGDIR = "DIR_TO_STORE_LOGS"
 CURTIMEDATE = time.strftime("%Y-%m-%d_%H.%M.%S")  # used to generate logname
-FTP_ERRORS_LIMIT = 3
+dlsuccess = False
 currentfile = " " # init current file variable used for reseting error counts
-lasterrorfile = "none"
-ftperrors = 0
 
 # setup logging - save log to the specifed log dir with the name of the
 # current time and a .txt extension, also only store info messages and above
@@ -26,7 +17,6 @@ logging.basicConfig(filename=LOGDIR + CURTIMEDATE + '.txt', level=logging.INFO)
 
 
 def connect_to_telaxftp():
-    global ftperrors
     # setup stfp connection
     try:
         cnopts = pysftp.CnOpts(knownhosts=KH)
@@ -35,89 +25,78 @@ def connect_to_telaxftp():
                                  password=TELAXPWD, cnopts=cnopts)
         return sftp
     except Exception as e:
-        # log the failure add to error count and relaunch the download
-        ftperrors += 1
+        # log the failure and relaunch the download
         logging.error(str(e))
         sftpdownload()
 
 
 def sftpdownload():
-    global ftperrors, currentfile, lasterrorfile 
-    if currentfile != lasterrorfile and ftperrors > 0:
-        print("Last file to cause an error does not match current file..... Resetting Error Counter")
-        logging.info("The last file to cause an error does not match current file.... resetting error counter")
-        ftperrors = 0
-    # check to make sure the error limit has not been reached
-    # if it has abort and send the log as an email.
-    if ftperrors >= FTP_ERRORS_LIMIT:
-        print('Error limit reached aborting!')
-        logging.error('Error limit reached aborting!')
-        sendemail(LOGDIR + CURTIMEDATE + '.txt', 'Error')
-        exit()
-    # call the connect function and store the result to call on
-    sftp = connect_to_telaxftp()
-    # save the list of files and directorys so you can work with them
-    remotedir = sftp.listdir()
-    try:
-        for file in remotedir:
-            currentfile = file
-            # Make sure the currently selected file is an actual file
-            # and not a directory then proceed to downlaod if TRUE.
+    global dlsuccess
+    while not dlsuccess: 
+        # call the connect function and store the result to call on
+        sftp = connect_to_telaxftp()
+        # save the list of files and directorys so you can work with them
+        remotedir = sftp.listdir()
+        try:
+            for file in remotedir:
+                currentfile = file
+                # Make sure the currently selected file is an actual file
+                # and not a directory then proceed to downlaod if TRUE.
             
-            if sftp.isfile(file):
-                # check if file is already downloaded locally
-                if os.path.exists(LOCALDIR + file):
-                    # get local file size and remote file size
-                    lfsize = os.path.getsize(LOCALDIR + file)
-                    rfsize = sftp.stat(file).st_size
-                    # check if local size and remote match and skip
-                    # the download and log the event if they do
-                    if lfsize == rfsize:
-                        print(file + ' already exist' +
+                if sftp.isfile(file):
+                    # check if file is already downloaded locally
+                    if os.path.exists(LOCALDIR + file):
+                        # get local file size and remote file size
+                        lfsize = os.path.getsize(LOCALDIR + file)
+                        rfsize = sftp.stat(file).st_size
+                        # check if local size and remote match and skip
+                        # the download and log the event if they do
+                        if lfsize == rfsize:
+                            print(file + ' already exist' +
                                      ' and matches remote file size')
-                        logging.info(file + ' already exist' +
+                            logging.info(file + ' already exist' +
                                             ' and matches remote file size')
-                    # if local and remote sizes don't match delete local file
-                    # and attempt to download the file again
-                    if lfsize != rfsize:
-                        lasterrorfile = file # update last file to cause error var
-                        print(file + ' has a size mismatch - ' +
+                        # if local and remote sizes don't match delete local file
+                        # and attempt to download the file again
+                        if lfsize != rfsize:
+                            lasterrorfile = file # update last file to cause error var
+                            print(file + ' has a size mismatch - ' +
                                      'local size is ' + str(lfsize) +
                                      ' and remote size is ' + str(rfsize))
-                        logging.info(file + ' has a size mismatch - ' +
+                            logging.info(file + ' has a size mismatch - ' +
                                             'local size is ' + str(lfsize) +
                                             ' and remote size is ' + str(rfsize))
-                        print('Removing corrupt file named ' + file)
-                        logging.info('Removing corrupt file named ' + file)
-                        os.remove(LOCALDIR + file)
-                        print('Redownloading ' + file + ' from server')
-                        logging.info('Redownloading ' + file + ' from server')
+                            print('Removing corrupt file named ' + file)
+                            logging.info('Removing corrupt file named ' + file)
+                            os.remove(LOCALDIR + file)
+                            print('Redownloading ' + file + ' from server')
+                            logging.info('Redownloading ' + file + ' from server')
+                            sftp.get(file, LOCALDIR + file)
+                            print('Download of ' + file + ' completed')
+                            logging.info('Download of ' + file + ' completed')
+                    else:
+                        # file does not exist locally proceed to download
+                        print('Downloading ' + file + ' from server')
+                        logging.info('Downloading ' + file + ' from server')
                         sftp.get(file, LOCALDIR + file)
                         print('Download of ' + file + ' completed')
                         logging.info('Download of ' + file + ' completed')
-                else:
-                    # file does not exist locally proceed to download
-                    print('Downloading ' + file + ' from server')
-                    logging.info('Downloading ' + file + ' from server')
-                    sftp.get(file, LOCALDIR + file)
-                    print('Download of ' + file + ' completed')
-                    logging.info('Download of ' + file + ' completed')
-        print('All downloads have been completed')
-        logging.info('All downloads have been completed')
-        # close the connection to the server
-        sftp.close()
-        logging.info('SFTP connection closed - Downloads Completed at ' +
+            print('All downloads have been completed')
+            logging.info('All downloads have been completed')
+            # close the connection to the server
+            sftp.close()
+            dlsuccess = True
+            logging.info('SFTP connection closed - Downloads Completed at ' +
                      time.strftime("%Y/%m/%d %H:%M:%S"))
-        # send email with the log file
-        sendemail(LOGDIR + CURTIMEDATE + '.txt', 'Success')
+            # send email with the log file
+            sendemail(LOGDIR + CURTIMEDATE + '.txt', 'Success')
 
-    except Exception as e:
-        # update error file with the file name that just failed
-        lasterrorfile = currentfile
-        logging.error(str(e))
-        # update error count and reattempt the downloads
-        ftperrors += 1
-        sftpdownload()
+        except Exception as e:
+            # update error file with the file name that just failed
+            lasterrorfile = currentfile
+            logging.error(str(e))
+            # reattempt the downloads
+            sftpdownload()
 
 
 def main():
